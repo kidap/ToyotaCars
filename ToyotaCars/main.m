bb62a21bb665dd352699aea0a32879660346f633
Karlo Pagtakhan <kidap@yahoo.com>
2016-01-06 09:55:14 -0500

    Initial Commit

---
+++ ToyotaCars/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ToyotaCars
+//
+//  Created by Karlo Pagtakhan on 01/06/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
